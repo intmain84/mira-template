e981c8c1671c5ef82aeedc69e7e61e4da958ba80
intmain <aheleha@gmail.com>
2026-05-20 01:35:25 +0500

    Updates

--- _next/static/qv4ihZsc01IO_vHwaTQcn/_buildManifest.js
+++ _next/static/qv4ihZsc01IO_vHwaTQcn/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mira-template//_next/:path+",
+        "destination": "/mira-template/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
